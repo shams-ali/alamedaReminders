633185dec6177e9592399ce5beb4a2c298333ce7
Selina Pirani <selinapirani@Selinas-MacBook-Pro.local>
2018-07-05 00:04:45 -0700

    (debug) env

--- helpers.js
+++ helpers.js
@@ -9,6 +9,7 @@ const helpers = {
     auth: new google.auth.JWT(client_email, null, private_key, ['https://www.googleapis.com/auth/plus.me', 'https://www.googleapis.com/auth/calendar']),
 };
 
+console.log(private_key, client_email)
 helpers.auth.authorizeAsync()
 console.log('authorized in helpers')
 
