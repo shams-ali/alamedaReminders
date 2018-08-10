3ba136003f9fffff4d91f494e711fe8feb768284
Shams Ali <shams.ali0216@gmail.com>
2018-08-10 02:10:37 -0700

    (fix) app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const initiateTwilioJob = require('./chron.js');
 
 console.log('initiateTwilioJob status', initiateTwilioJob.running);
 
-app.get('/', () => res.send('There is no client for this, You have reached Alameda Reminders'))
+app.get('/', (_, res) => res.send('There is no client for this, You have reached Alameda Reminders'))
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
 
