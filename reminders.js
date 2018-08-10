bbfb2d236122605f7560d4d84d4a03ae2bb02a7f
Shams Ali <shams.ali0216@gmail.com>
2018-08-10 01:47:27 -0700

    (clean) events

--- reminders.js
+++ reminders.js
@@ -1,51 +1,43 @@
 require('dotenv').config();
 
 const {
-    CALENDAR_ID: calendarId,
     TWILIO_ACCOUNT_SID: accountSid,
     TWILIO_AUTH_TOKEN: authToken,
-    TWILIO_PHONE: twilioPhone
+    TWILIO_PHONE: twilioPhone,
+    TEST_PHONE
 } = process.env;
 
-const { promisifyAll, mapSeries } = require('bluebird');
+const { mapSeries } = require('bluebird');
 const Moment = require('moment');
 
-const { auth } = require('./helpers');
 const contacts = require('./contacts');
-
-const { google: { calendar } } = require('googleapis');
-const { events } = calendar('v3');
-[events].forEach(promisifyAll);
-
+const events = require('./events.js')
 const { messages } = require('twilio')(accountSid, authToken);
+console.log('hello world');
 
 const initiateTwilio = async() => {
     try {
         console.log('twilio initiated')
-        await auth.authorizeAsync();
         console.log('authorized')
-        const tomorrow = new Moment().add(1, 'days');
-        const { data: { items = [] } = {} } = await events.listAsync({
-            auth,
-            calendarId,
-            singleEvents: true,
-            timeMin: tomorrow.format(),
-            timeMax: tomorrow.add(1, 'days').format(),
-            orderBy: 'startTime'
-        });
-        console.log(items, 'this is items')
-        const twilioMessages = items.reduce((messages, { summary, description = '', attendees = [], start: { dateTime } = {} }) =>
-            messages.concat(attendees.map(({ email }) => ({ 
-                from: twilioPhone, 
-                body: `REMINDER: ${summary.trim()} at ${Moment(dateTime).format('LLLL')} *${description}*`, 
-                to: `+1${contacts[email]}`
+        const { Alameda } = events 
+        const twilioMessages = Alameda
+            .filter(({date}) => Moment(date).isSame(Date.now(), 'day'))
+            .reduce((messages, { date, occasion, items }) =>
+                messages.concat(items.map(({ email, item }) => ({ 
+                    from: twilioPhone, 
+                    body: `REMINDER: ${item} at ${Moment(date).format('LLLL')} *${occasion}*`, 
+                    to: `+1${contacts[email]}`
+                })
+            )), [])
+            .concat({
+                from: '16502002692',
+                body: 'khane notificatione sent',
+                to: `+${TEST_PHONE}`
             })
-        )), [])
         
         console.log(twilioMessages, 'this is twilio messages')
         const results = await mapSeries(twilioMessages, message => messages.create(message))
         console.log(results, 'this is results')
-
     } catch (error) {
         console.error('this is error', error);
     }
